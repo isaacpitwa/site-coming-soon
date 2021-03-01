f5c37935276be4ce6767680a6445ba4996945dc4
Isaac Pitwa <isaacpitwa256@gmail.com>
2021-03-01 12:22:40 +0300

    Restructure

--- src/views/paymentLinks/RightPath.jsx
+++ src/views/paymentLinks/RightPath.jsx
@@ -3,13 +3,13 @@ import React from 'react'
 
 export default function RightPath() {
     return (
-       <Box p="6">
-           <Flex align="center" px="8" pb="4">
+       <Box p="4">
+           <Flex align="center" px="8" pb="2">
                <Link href="https://right-path.org/">right-path.org</Link>
                <Spacer/>
                <Text size="sm">+256 414 373 968 / +256 775 738282</Text>
            </Flex>
-           <Box  w="100%" h="90vh"  bgImage="linear-gradient(rgba(18, 18, 44, 0.8), rgba(18, 18, 44, 0.8)), url(https://cdn.filestackcontent.com/TmUstxRASJyhlKy9RwSL);">
+           <Box  w="100%" h="89vh" background= 'url(/static/img/donation-bg@3x.png) center no-repeat' backgroundSize= 'cover'  bgImage="linear-gradient(rgba(18, 18, 44, 0.8), rgba(18, 18, 44, 0.8)), url(https://cdn.filestackcontent.com/TmUstxRASJyhlKy9RwSL);">
 
            </Box>
        </Box>
