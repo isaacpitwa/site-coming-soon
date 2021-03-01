030aaa9915acce4b47853d9f288788c97c86de33
Isaac Pitwa <isaacpitwa256@gmail.com>
2021-03-01 13:14:18 +0300

    set up form

--- src/components/rightPath/DonationForm.jsx
+++ src/components/rightPath/DonationForm.jsx
@@ -38,12 +38,12 @@ export default function DonationForm() {
             >
                 {(props) => (
                     <Form>
-                        <SimpleGrid columns={2} spacing={10}>
+                        <SimpleGrid columns={2} spacing={4}>
                             <Field name="fName" >
                                 {({ field, form }) => (
                                     <FormControl isInvalid={form.errors.fName && form.touched.fName}>
-                                        <FormLabel htmlFor="fName"  fontSize="sm" fontWeight="400">First name</FormLabel>
-                                        <Input {...field} id="fName" placeholder="First Name"   fontSize="sm" fontWeight="400"/>
+                                        <FormLabel htmlFor="fName" fontSize="sm" fontWeight="400">First name</FormLabel>
+                                        <Input {...field} id="fName" placeholder="First Name" fontSize="sm" fontWeight="400" />
                                         <FormErrorMessage>{form.errors.fName}</FormErrorMessage>
                                     </FormControl>
                                 )}
@@ -51,19 +51,19 @@ export default function DonationForm() {
                             <Field name="lName" >
                                 {({ field, form }) => (
                                     <FormControl isInvalid={form.errors.lName && form.touched.lName}>
-                                        <FormLabel htmlFor="lName"  fontSize="sm" fontWeight="400">First name</FormLabel>
-                                        <Input {...field} id="lName" placeholder="Last Name"  fontSize="sm" fontWeight="400" />
+                                        <FormLabel htmlFor="lName" fontSize="sm" fontWeight="400">First name</FormLabel>
+                                        <Input {...field} id="lName" placeholder="Last Name" fontSize="sm" fontWeight="400" />
                                         <FormErrorMessage>{form.errors.lName}</FormErrorMessage>
                                     </FormControl>
                                 )}
                             </Field>
                         </SimpleGrid>
-                        <SimpleGrid columns={2} spacing={10}>
+                        <SimpleGrid columns={2} spacing={4}>
                             <Field name="email">
                                 {({ field, form }) => (
                                     <FormControl pt="2" isInvalid={form.errors.email && form.touched.email} isRequired>
-                                        <FormLabel htmlFor="email"  fontSize="sm" fontWeight="400">Email</FormLabel>
-                                        <Input {...field} type="email" id="email"   fontSize="sm" fontWeight="400"/>
+                                        <FormLabel htmlFor="email" fontSize="sm" fontWeight="400">Email</FormLabel>
+                                        <Input {...field} type="email" id="email" fontSize="sm" fontWeight="400" />
                                         <FormErrorMessage>{form.errors.email}</FormErrorMessage>
                                     </FormControl>
                                 )}
@@ -71,8 +71,8 @@ export default function DonationForm() {
                             <Field name="phoneNumber" isRequired>
                                 {({ field, form }) => (
                                     <FormControl pt="2" isInvalid={form.errors.phoneNumber && form.touched.phoneNumber}>
-                                        <FormLabel htmlFor="phoneNumber"  fontSize="sm" fontWeight="400">Phone Number</FormLabel>
-                                        <Input {...field} type="text" id="phoneNumber" placeholder="256 123456789"   fontSize="sm" fontWeight="400"/>
+                                        <FormLabel htmlFor="phoneNumber" fontSize="sm" fontWeight="400">Phone Number</FormLabel>
+                                        <Input {...field} type="text" id="phoneNumber" placeholder="256 123456789" fontSize="sm" fontWeight="400" />
                                         <FormErrorMessage>{form.errors.phoneNumber}</FormErrorMessage>
                                     </FormControl>
                                 )}
@@ -81,8 +81,8 @@ export default function DonationForm() {
                         <Field name="amount">
                             {({ field, form }) => (
                                 <FormControl pt="2" isInvalid={form.errors.amount && form.touched.amount} >
-                                    <FormLabel htmlFor="amount"  fontSize="sm" fontWeight="400">Amount(USD)</FormLabel>
-                                    <Input {...field} type="number" id="amount" placeholder="0.00"  fontSize="sm" fontWeight="400"/>
+                                    <FormLabel htmlFor="amount" fontSize="sm" fontWeight="400">Amount(USD)</FormLabel>
+                                    <Input {...field} type="number" id="amount" placeholder="0.00" fontSize="sm" fontWeight="400" />
                                     <FormErrorMessage>{form.errors.amount}</FormErrorMessage>
                                 </FormControl>
                             )}
@@ -96,34 +96,37 @@ export default function DonationForm() {
                                 </FormControl>
                             )}
                         </Field>
-                        <SimpleGrid columns={2} spacing={10}>
-                        <Field name="expiryMonth" isRequired>
-                            {({ field, form }) => (
-                                <FormControl pt="4" isInvalid={form.errors.expiryMonth && form.touched.expiryMonth} isRequired>
-                                    <FormLabel htmlFor="expiryMonth" fontSize="sm" fontWeight="400"> Expiry Month</FormLabel>
-                                    <Input {...field} type="number" id="expiryMonth" placeholder="12" fontSize="sm" fontWeight="400" />
-                                    <FormErrorMessage>{form.errors.expiryMonth}</FormErrorMessage>
-                                </FormControl>
-                            )}
-                        </Field>
-                        <Field name="expiryYear" isRequired>
-                            {({ field, form }) => (
-                                <FormControl pt="4" isInvalid={form.errors.expiryYear && form.touched.expiryYear} isRequired>
-                                    <FormLabel htmlFor="expiryYear" fontSize="sm" fontWeight="400"> Expiry Year</FormLabel>
-                                    <Input {...field} type="number" id="expiryYear" placeholder="2022" fontSize="sm" fontWeight="400" />
-                                    <FormErrorMessage>{form.errors.expiryYear}</FormErrorMessage>
-                                </FormControl>
-                            )}
-                        </Field>
+                        <SimpleGrid columns={2} spacing={4}>
+                            <Field name="expiryMonth" isRequired>
+                                {({ field, form }) => (
+                                    <FormControl pt="4" isInvalid={form.errors.expiryMonth && form.touched.expiryMonth} isRequired>
+                                        <FormLabel htmlFor="expiryMonth" fontSize="sm" fontWeight="400"> Expiry Month</FormLabel>
+                                        <Input {...field} type="number" id="expiryMonth" placeholder="12" fontSize="sm" fontWeight="400" />
+                                        <FormErrorMessage>{form.errors.expiryMonth}</FormErrorMessage>
+                                    </FormControl>
+                                )}
+                            </Field>
+                            <Field name="expiryYear" isRequired>
+                                {({ field, form }) => (
+                                    <FormControl pt="4" isInvalid={form.errors.expiryYear && form.touched.expiryYear} isRequired>
+                                        <FormLabel htmlFor="expiryYear" fontSize="sm" fontWeight="400"> Expiry Year</FormLabel>
+                                        <Input {...field} type="number" id="expiryYear" placeholder="2022" fontSize="sm" fontWeight="400" />
+                                        <FormErrorMessage>{form.errors.expiryYear}</FormErrorMessage>
+                                    </FormControl>
+                                )}
+                            </Field>
                         </SimpleGrid>
                         <Button
                             mt={4}
-                            colorScheme="teal"
+                           backgroundColor="#F5A623"
+                           color="white"
+                            width="100%"
+                            borderRadius="sm"
+                            fontSize="sm"
                             isLoading={props.isSubmitting}
                             type="submit"
-                        >
-                            Submit
-          </Button>
+                            _hover={{opacity:'0.8'}}
+                        >DONATE ONCE</Button>
                     </Form>
                 )}
             </Formik>
