93eef763cc3fbe2b415077adb42c5b95e614f51d
Isaac Pitwa <isaacpitwa256@gmail.com>
2021-03-02 13:19:18 +0300

    Content for transaction

--- src/components/result/success.jsx
+++ src/components/result/success.jsx
@@ -1,9 +1,10 @@
+import { Box,Text } from '@chakra-ui/react'
 import React from 'react'
 
 export default function SuccessTransaction() {
     return (
-        <div>
-            
-        </div>
+       <Box d="flex" alignItems="center" justifyContent="center">
+           <Text>Transaction Successful</Text>
+       </Box>
     )
 }
