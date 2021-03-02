2299732efc75fb9876361798ea39b3cba4a8a645
Isaac Pitwa <isaacpitwa256@gmail.com>
2021-03-02 13:40:07 +0300

    page polishing

--- src/components/result/success.jsx
+++ src/components/result/success.jsx
@@ -1,6 +1,6 @@
 import { Box, Heading,Image,Text,Icon, Center, Button } from '@chakra-ui/react'
 import React from 'react'
-import {GoMailRead} from 'react-icons/go'
+import {IoCheckmarkCircleOutline} from 'react-icons/io5'
 import { Link } from 'react-router-dom'
 import * as ROUTES from '../../utils/constants/routes'
 
@@ -10,13 +10,13 @@ export default function SuccessTransaction() {
         <Image src="/images/logo.png" pb="8"/>
         <Center pt="16" pb="4">
             <Box w="20" h="20" bg="#fff4e6" d="flex" alignItems="center" justifyContent="center" borderRadius="lg">
-                <Icon as ={GoMailRead} boxSize="10" color="brandColor"/>
+                <Icon as ={IoCheckmarkCircleOutline} boxSize="10" color="#F5A623"/>
             </Box>
         </Center>
-        <Heading size="sm" pt="4">Check your Email</Heading>
-        <Text fontSize="sm" color="GrayText" pt="2">We have sent  Email verification  Instructions<br/>to your email</Text>
-        <Button  type="submit" bgColor="brandColor" color="white" size="md" mt="4">Open email</Button>
-        <Text fontSize="md" color="GrayText" pt="36">Did not recieve email, check spam filter, <br/> or  <Link to={ROUTES.LOGIN} style={{color:'blue'}}>Login using email address</Link></Text>
+        <Heading size="sm" pt="4">Transactions Successful</Heading>
+        <Text fontSize="sm" color="GrayText" pt="2">We have sent  Email Transaction  Details<br/>to your email</Text>
+        <Button  type="submit" bgColor="#F5A623" color="white" size="md" mt="4">Go Back</Button>
+        <Text fontSize="md" color="GrayText" pt="36">Did not recieve email, check spam filter, <br/> or  <Link to={ROUTES.RIGHT_PATH_PAYMENT_LINK} style={{color:'blue'}}>Donate again</Link></Text>
     </Box>
     )
 }
